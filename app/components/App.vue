47d8ebbbdfd39ad57d7d5759bd5daa3d4ab61159
hk698 <khanhasnaat111@gmail.com>
2021-03-18 13:24:04 +0000

    root component

---
+++ app/components/App.vue
@@ -0,0 +1,74 @@
+<template>
+   <Page>
+       <ActionBar title="Classes and Activities"/>
+       
+       
+<StackLayout>
+       <BottomNavigation height="400px">
+        <TabStrip>
+            <TabStripItem>
+                <Label text="Home"></Label>
+                <Image src="res://home"></Image>
+            </TabStripItem>
+            <TabStripItem>
+                <Label text="Cart"></Label>
+                <Image src="res://settings"></Image> 
+            </TabStripItem>  
+        </TabStrip>
+        <TabContentItem>
+           <LessonList :lessons="lessons" @addToCart="addToCart"/>
+        </TabContentItem>
+        <TabContentItem>
+            <Checkout :cart="cart" @remove="remove" @emptyCart="emptyCart"/>
+        </TabContentItem>
+    </BottomNavigation>
+</StackLayout>
+
+   </Page>
+</template>
+
+
+<script>
+import Checkout from './Checkout.vue';
+import LessonList from './LessonList.vue';
+export default {
+  components: { LessonList, Checkout },
+    data() {
+	    return {
+            lessons: [],
+            cart:[],
+	    };
+    },
+    methods:{
+        accessLessons(){
+            fetch('https://individialwork2.herokuapp.com/lessons') 
+                .then(response => response.json()) 
+                .then(data => this.lessons = data) 
+                .catch(error => console.log(error));
+        },
+        addToCart(lesson){
+            lesson.availableSpaces--;
+            this.cart.push(lesson);
+        },
+        remove(lesson ,index){
+            this.cart.splice(index, 1);  
+            lesson.availableSpaces++;
+
+        },
+        emptyCart(){
+            this.cart = [];
+        }
+    },
+    mounted(){
+        this.accessLessons();
+    },
+    computed:{
+        noCartItems() {  
+            return this.cart.length;
+        },
+    }
+}
+</script>
+
+<style>
+</style>
